379e6e6dc7c2ff393b9923dfef839ae4e759cc08
fansha <julfanshahidayah@gmail.com>
2023-09-26 12:40:46 +0700

    ocd?

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,10 +3,10 @@ import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/modules/auth/strategy/jwt-auth.guard';
 import { RolesGuard } from 'src/modules/auth/strategy/roles-auth.guard';
 import { Role } from 'src/decorators/role.decorator';
+import { RolesEnum } from 'src/models/roles.model';
 import { ApiResource } from 'src/resources/api.resource';
 import { UsersResource } from 'src/resources/users/users.resource';
 import { UsersCollection } from 'src/resources/users/users.collection';
-import { RolesEnum } from 'src/models/roles.model';
 
 @Controller('api/users')
 export class UsersController {
